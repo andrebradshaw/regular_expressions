6096de9aed478e6b8e294931c47eb7b3a75dc3bf
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-07-10 22:04:38 -0400

    Update words_NEAR_otherwords.js

--- words_NEAR_otherwords.js
+++ words_NEAR_otherwords.js
@@ -1,3 +1,7 @@
+/*/
+-------- follow along at -------> https://youtu.be/kMbw4uIiWQo
+/*/
+
   var wordarr = ["application","sql","database"]; //words to match
   var readString = 'Develop interface applications to convert flat files sent by clients to load to our system SQL server database to be worked by our service representatives. Develop interface applications to pull data from our SQL server database; convert into flat files and then return to our clients utilizing Microsoft.Net technology and OOP Design practices and principles.'; //this will be filled with the text we want to match
   //this is not likely the best way to do this, but it uses regular expressions, so why not go with it?
